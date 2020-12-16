4de3a1ce9d4b70cdabe3ae75fc7d8f9d44e1edc0
Boutroue Guillaume <g.boutroue@free.fr>
2020-12-17 00:55:50 +0100

    etape3

---
+++ php/nav.php
@@ -0,0 +1,8 @@
+	<link rel="stylesheet" href="/cours/biblio_groupe8_etape3/biblio_groupe8_etape2/style/biblioteque.css">
+<nav>
+ 	<ul>
+    <li><a href="bibliotheque.php">Accueil</a></li>
+    <li><a href="php/contact.php">Nous contacter</a></li>
+  </ul>
+
+</nav>
---
+++ php/receive.php
@@ -0,0 +1,13 @@
+<?php
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $name = $_POST['client_name'];
+   $type = $_POST['client_type'];
+    $mail = $_POST['client_type'];
+   if(empty($name)){
+     echo"Vous n'avez pas renseigné votre nom.";
+   }else{
+     echo "Bonjour $name !
+     Merci d'avoir répondu à ce petit formulaire !";
+   }
+ }
+ ?>
