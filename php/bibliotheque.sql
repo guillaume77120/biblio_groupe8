a8ccc306c02aaf0493f3accd97b254ca89a7b787
guillaume77120 <75263333+guillaume77120@users.noreply.github.com>
2021-01-08 15:16:14 +0100

    Add files via upload

---
+++ php/bibliotheque.sql
@@ -0,0 +1,265 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 08, 2021 at 02:15 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auteur`
+--
+
+CREATE TABLE `auteur` (
+  `idPersonne` int(11) NOT NULL,
+  `idLivre` varchar(15) NOT NULL,
+  `idRole` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `auteur`
+--
+
+INSERT INTO `auteur` (`idPersonne`, `idLivre`, `idRole`) VALUES
+(4, '9791094169261', 1),
+(3, '9782344014196', 1),
+(3, '9782344001684', 1),
+(3, '9782344001653', 1),
+(1, '9782298035148', 1),
+(3, '9782344001660', 1),
+(1, '9782226186072', 1),
+(2, '9782092544372', 1),
+(2, '9782092538036', 1),
+(2, '9782092530870', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `editeur`
+--
+
+CREATE TABLE `editeur` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `editeur`
+--
+
+INSERT INTO `editeur` (`id`, `libelle`) VALUES
+(1, 'Albin Michel'),
+(2, 'Nathan'),
+(3, 'Glénat'),
+(4, 'Graph Zeppelin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`id`, `libelle`) VALUES
+(1, 'Bandes dessinés'),
+(3, 'Essai'),
+(2, 'Roman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `langue`
+--
+
+CREATE TABLE `langue` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `langue`
+--
+
+INSERT INTO `langue` (`id`, `libelle`) VALUES
+(1, 'Français');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `livre`
+--
+
+CREATE TABLE `livre` (
+  `isbn` varchar(15) NOT NULL,
+  `titre` varchar(500) NOT NULL,
+  `editeur` int(11) NOT NULL,
+  `annee` int(11) DEFAULT NULL,
+  `genre` int(11) DEFAULT NULL,
+  `langue` int(11) DEFAULT NULL,
+  `nbpages` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `livre`
+--
+
+INSERT INTO `livre` (`isbn`, `titre`, `editeur`, `annee`, `genre`, `langue`, `nbpages`) VALUES
+('9782092530870', 'Les douze travaux d\'Hercule', 2, 2011, 1, 1, 64),
+('97820925380360', 'L\'enlèvement de Perséphone', 2, 2012, 1, 1, 64),
+('978209254437', 'La malédiction d\'Œdipe', 2, 2013, 1, 1, 64),
+('9782226186072', 'Percy Jackson : Le voleur de foudre', 1, 2008, 2, 1, 480),
+('9782298035148', 'Percy Jackson la mer des monstres', 1, 2010, 2, 1, 320),
+('9782344001653', 'L\'Odyssée', 3, 2017, 1, 1, 56),
+('9782344001660', 'L\'Illiade', 3, 2016, 1, 1, 56),
+('9782344001684', 'Jason et la toison d\'or ', 3, 2017, 1, 1, 56),
+('9782344014196', 'Dédale et Icare', 3, 2018, 1, 1, 56),
+('9791094169261', 'Achille, la naissance d\'un héro', 4, 2018, 1, 1, 48);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prenom` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `personne`
+--
+
+INSERT INTO `personne` (`id`, `nom`, `prenom`) VALUES
+(1, 'Riordian', 'Rick'),
+(2, 'de Montarde', 'Hélène'),
+(3, 'Ferry', 'Luc'),
+(4, 'Ferri', 'Casimo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `libelle`) VALUES
+(1, 'Ecrivain'),
+(2, 'Illustrateur'),
+(3, 'Traducteur'),
+(4, 'Préface');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `editeur`
+--
+ALTER TABLE `editeur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `libelle` (`libelle`);
+
+--
+-- Indexes for table `langue`
+--
+ALTER TABLE `langue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `livre`
+--
+ALTER TABLE `livre`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Indexes for table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `editeur`
+--
+ALTER TABLE `editeur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `langue`
+--
+ALTER TABLE `langue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
